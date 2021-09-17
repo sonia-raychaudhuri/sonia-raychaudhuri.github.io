7fcad70d8bb3d16f6415d0fe3df9f92f6fbf547b
sraychau <sraychau@sfu.ca>
2021-09-17 11:26:42 -0700

    First draft

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function()
+{
+    $("#law21").hover(
+        function()
+        {
+            $(this).attr("src", "images/publications/law2021/teaser.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "images/publications/law2021/teaser.png");
+        }                         
+    );                  
+});
